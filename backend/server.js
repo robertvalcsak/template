fd9affa9fb7944b692e18b8b4a9e59b414caebb6
Róbert Valcsák <robertvalcsak93@gmail.com>
2022-11-03 12:54:49 +0100

    11-03 órai munka folytatása

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const fileUpload = require("express-fileUpload")
 const express = require('express')
 const app = express()
 const port = 3000
@@ -8,6 +9,10 @@ const path = require("path");
 const fs = require("fs")
 
 app.use(express.json())
+app.use(fileUpload())
+
+// kép küldése csak formdata használatával lehetséges
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/index.html`))
@@ -25,6 +30,24 @@ app.post("/upload", (req, res) => {
     })
    
 })  //endpoint, frontendet hozzá kell igazítani
+
+app.post("/upload-image", (req, res) => {
+    if(!req.files){
+        return res.status(400).send("No files were uploaded")
+    }
+    const picture = req.files.file
+    const picName = req.body.fileName
+    console.log(picName);
+
+    picture.mv(`${__dirname}/data/${picName}.jpg`, (error) => {
+        if(error){
+            console.log(error);
+            return res.status(500).send(error)
+        }else{
+            res.status(200).send("Image OK")
+        }
+    })   // mv = move
+});
 
 // app.get('/style.css', (req, res) => {
 //     res.sendFile(path.join(`${__dirname}/../frontend/style.css`))
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -6,11 +6,21 @@ form.addEventListener("submit", function(event){
         category : document.querySelector("#category").value,
         price : parseInt(document.querySelector("#price").value)
     };
+
+    const formData = new formData();
+    formData.append("fileName", document.querySelector("#file-name".value))
+    formData.append("file", document.querySelector("#file").files[0]) // files array-t ad vissza, még ha egy elem is található benne
+
     fetch("/upload", {
         method: 'POST', 
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(product),
+    });
+
+    fetch("/upload-image", {
+        method : 'POST',
+        body : formData
     });
 });
